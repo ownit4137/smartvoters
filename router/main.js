12036e6047fe777b725c4bd91a26ec89c4d72aa7
cow-coding <kbp0237@gmail.com>
2021-06-17 12:13:59 +0900

    candidate API, member API

--- router/main.js
+++ router/main.js
@@ -3,6 +3,7 @@ module.exports = function (app, conn) {
     res.render("index.html");
   });
 
+  // PollPlace API
   app.get("/PollPlace", function (req, res) {
     var sql = 'SELECT DISTINCT sgId FROM election_code;'
 
@@ -15,32 +16,6 @@ module.exports = function (app, conn) {
         res.render("PollPlace.html", { codes: resultArray });
       }
     })
-  });
-
-  app.get("/CandInfo", function (req, res) {
-    res.render("CandInfo.html");
-  });
-
-  app.get("/CandProm", function (req, res) {
-    res.render("CandProm.html");
-  });
-
-  app.get("/Member", function (req, res) {
-    res.render("Member.html");
-  });
-
-  app.get("/PrevElec", function (req, res) {
-    var stmt = "SELECT * FROM election_code;";
-
-    conn.query(stmt, function (err, result) {
-      if (err) {
-        console.log(err);
-      } else {
-        var resultArray = Object.values(JSON.parse(JSON.stringify(result)));
-
-        res.render("PrevElec.html", { codes: resultArray });
-      }
-    });
   });
 
   app.get("/PollPlace_do", function (req, res) {
@@ -78,23 +53,80 @@ module.exports = function (app, conn) {
     }
   })
 
-  app.get("/getCandidate", function (req, res) {
-    var name = req.query.name;
-    var partyName = req.query.partyName;
-    var sql =
-      "SELECT * FROM candidate WHERE name LIKE '%" +
-      name +
-      "%' and partyName LIKE '%" +
-      partyName +
-      "%';";
+  // PollPlace CandidateInfo API
+  app.get("/CandInfo", function (req, res) {
+    var sql = 'SELECT DISTINCT sgId FROM election_code;'
 
     conn.query(sql, function (err, result) {
+      if (err) {
+        console.log(err);
+      }else{
+        var resultArray = Object.values(JSON.parse(JSON.stringify(result)));
+
+        res.render("CandInfo.html", { codes: resultArray });
+      }
+    })
+  });
+
+  app.get("/CandInfo_do", function (req, res) {
+    var sgId = req.query.date;
+    var sggName = req.query.elecplace
+    var name = req.query.name;
+
+    var sql = 'SELECT DISTINCT sgId FROM election_code; '
+    var sql1 = "SELECT * FROM candidate WHERE sgId="+sgId+" AND sggName LIKE '%"+sggName+"%' AND name LIKE '%"+name+"%' ORDER BY giho ASC; ";
+
+    conn.query(sql + sql1, function (err, result) {
+      if (err) {
+        console.log(err);
+      }else {
+        var resultArray1 = Object.values(JSON.parse(JSON.stringify(result[0])));
+        var resultArray2 = Object.values(JSON.parse(JSON.stringify(result[1])));
+
+        res.render("CandInfo.html", { codes : resultArray1, candList : resultArray2 })
+      }
+    })
+  })
+
+  // Candidate Promise API
+  app.get("/CandProm", function (req, res) {
+    res.render("CandProm.html");
+  });
+
+  // Politician Member API
+  app.get("/Member", function (req, res) {
+    res.render("Member.html");
+  });
+
+  app.get("/Member_do", function (req, res) {
+    var name = req.query.name;
+    var partyName = req.query.partyName;
+
+    var sql = "SELECT * FROM politician WHERE name LIKE '%"+name+"%' AND partyName LIKE '%"+partyName+"%' ORDER BY name ASC; ";
+
+    conn.query(sql, function (err, result) {
+      if (err) {
+        console.log(err);
+      }else {
+        console.log(sql);
+        var resultArray = Object.values(JSON.parse(JSON.stringify(result)));
+
+        res.render("Member.html", { memberList : resultArray })
+      }
+    })
+  })
+
+  // Previous Election API
+  app.get("/PrevElec", function (req, res) {
+    var stmt = "SELECT * FROM election_code;";
+
+    conn.query(stmt, function (err, result) {
       if (err) {
         console.log(err);
       } else {
         var resultArray = Object.values(JSON.parse(JSON.stringify(result)));
 
-        res.render("poll.html", { candidate: resultArray });
+        res.render("PrevElec.html", { codes: resultArray });
       }
     });
   });
